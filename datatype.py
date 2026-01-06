c5346b33994c4b3886d43ef8d5672f445a149049
Ansohmyous <sohamruz1998@gmail.com>
2026-01-06 19:28:57 +0530

    datatype.py published
    Day2 Program published

---
+++ datatype.py
@@ -0,0 +1,7 @@
+#Type Function: This function will tell the type of data passed as an arg inside the type function
+
+# Type function usage 
+print(type(123))
+print(type("Hello"))
+print(type(123.456))
+print(type(False))
